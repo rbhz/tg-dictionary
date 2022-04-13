85485f6028db2be2e61be954dc6a1811ed14cc4e
Kirill Sorokin <k.sorokin@getorca.app>
2022-04-14 02:17:53 +0300

    Update item response template

--- app/bot/templates.go
+++ app/bot/templates.go
@@ -1,14 +1,13 @@
 package bot
 
 const DictionaryItemTemplate = `<b>{{ .Item.Word }}</b>
-<u>Phonetics</u>: {{ .Item.Phonetics.Text }}
-
 <b>Meanings:</b>
 {{- range $m := .Item.Meanings }}
-<code>{{ $m.Definition }} ({{ $m.PartOfSpeech }})</code>
+<code>{{ $m.Definition }}</code> ({{ $m.PartOfSpeech }})
 {{- range $e := $m.Examples }}
 {{ $e }}
 {{- end }}
 ___
 {{- end }}
+<u>Phonetics</u>: {{ .Item.Phonetics.Text }}
 `
